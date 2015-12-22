d7f55bb26ab4d6445cc9cb61ea2446a17ab8d014
RizOrt218 <rizhiafalen@gmail.com>
2015-12-22 11:22:42 -1000

    first commit? appended an image into my mainContent

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,10 @@
 
 window.onload = function () {
   $.getJSON( './api/get_the_app.json', function(data) { //retrieve jSon file. created fn to load data
-    var title = data.data.children[0].data.title; //the title of the JSON fil.
+    console.log(data);
 
-    var feed = $( 'body' ).append( '<div>' ); //creates div before closing body tag
-    $(feed).append(title); //takes div
-
+    // var thumbnail = data.data.children[0].data.thumbnail;
+    // $('.mainContent').append( '<img src=" '+ thumbnail +' " />' );
 
   });
 
