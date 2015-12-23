f21937bfa603a104af48bea8c7c9d76d757761dc
RizOrt218 <rizhiafalen@gmail.com>
2015-12-22 18:55:12 -1000

    added divs, modified scss

--- public/js/app.js
+++ public/js/app.js
@@ -1,44 +1,46 @@
 
 
 window.onload = function () {
-  $.getJSON( './api/random.json', function(data) { //retrieve jSon file. created fn to load data
+  $.getJSON( './api/random.json', function(data) {
     console.log(data);
 
 //================ data ===========================
-    var dataChild = data.data.children;
-    var thumbnail = dataChild[0].data.thumbnail;
-    var title = dataChild[0].data.title;
-    var description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quos nostrum itaque, quae ipsa, obcaecati, nemo rerum facere eos veritatis iure accusantium esse aut illo iste eligendi tempora natus consectetur.";
+  var randomButton = Math.floor(Math.random()*50);
+  var dataChild = data.data.children;
+  var thumbnail = dataChild[randomButton].data.thumbnail;
+  var title = dataChild[randomButton].data.title;
+  var description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quos nostrum itaque, quae ipsa, obcaecati, nemo rerum facere eos veritatis iure accusantium esse aut illo iste eligendi tempora natus consectetur.";
 //=================================================
-    var contentBlock = $('<div>'); //div for one whole block
-      contentBlock
-        .addClass('contentBlock')
-      ;
 
-    var conImage = $('<div><img src="'+thumbnail+'" /></div>');
-      conImage
-        .addClass( 'conImage' )
-      ;
-      contentBlock.append(conImage);
+  var contentBlock = $('<div>'); //div for one whole block
+    contentBlock
+      .addClass('contentBlock')
+    ;
 
-    var conTitle = $('<div><h3>conTitle</h3></div>');
-      conTitle
-        .addClass( 'conTitle' )
-      ;
-      contentBlock.append(title);
+  var conImage = $('<div><img src="'+thumbnail+'" /></div>');
+    conImage
+      .addClass( 'conImage' )
+    ;
+    contentBlock.append(conImage);
 
-    var conAuthor = $('<div>');
-      conAuthor
-        .addClass('conAuthor')
-      ;
-      contentBlock.append(conAuthor, "just a placement");
+  var conTitle = $('<div><h3>conTitle</h3></div>');
+    conTitle
+      .addClass( 'conTitle' )
+    ;
+    contentBlock.append(title);
 
-    var conDescription = $('<div>');
-      contentBlock.append(description);
+  var conAuthor = $('<div>');
+    conAuthor
+      .addClass('conAuthor')
+    ;
+    contentBlock.append(conAuthor, "just a placement");
 
-    $('.mainContent').append(contentBlock); //appending whole block in main
+  var conDescription = $('<div>');
+    contentBlock.append(description);
+
+  $('.mainContent').append(contentBlock); //appending whole block in main
 
 
-  });
+});
 
 };
